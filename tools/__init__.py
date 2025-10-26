a12fd65495491934da6bb27ea0c595f00da53b9a
pragyanaischool <75975970+pragyanaischool@users.noreply.github.com>
2025-10-26 21:53:50 +0530

    Create __init__.py

---
+++ tools/__init__.py
@@ -0,0 +1,9 @@
+"""
+This file makes the 'tools' directory a Python package.
+This allows the main app.py to import modules from this directory, like:
+
+from tools.document_reader_tool import DocumentReaderTool
+from tools.rag_manager import get_rag_tool
+"""
+# This file can be left empty.
+
